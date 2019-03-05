d32f5153e060b9d99738ebecbb43b1ae4c64af42
Luigi Hu <luigihu1@gmail.com>
2019-03-05 09:40:33 +0100

    Eliminato file java Hello, Creato file java somma, aggiunto Hello cpp

---
+++ Somma.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+ int somma;
+
+ System.out.println("Scrivi due numeri interi");
+ System.out.print("a :\t");
+ int a = scanner.nextInt
+ System.out.print("b :\t");
+ int b = scanner.nextInt();
+
+ somma = a+b;
+ System.out.print("La somma dei due numeri è ");
+ System.out.println(somma);
+ } 
